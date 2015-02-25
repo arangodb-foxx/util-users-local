c7a675dd32a15ce3a9915cfafe1e45bab7a4f7b0
Alan Plum <me@pluma.io>
2015-02-25 13:02:01 +0100

    Cleanup for 2.5.

--- errors.js
+++ errors.js
@@ -1,22 +1,20 @@
 /*global exports */
-(function () {
-  'use strict';
-  function UserNotFound(uid) {
-    this.message = 'User with user id ' + uid + ' not found.';
-    var err = new Error(this.message);
-    err.name = this.constructor.name;
-    this.stack = err.stack;
-  }
-  UserNotFound.prototype = new Error();
+'use strict';
+function UserNotFound(uid) {
+  this.message = 'User with user id ' + uid + ' not found.';
+  var err = new Error(this.message);
+  err.name = this.constructor.name;
+  this.stack = err.stack;
+}
+UserNotFound.prototype = new Error();
 
-  function UsernameNotAvailable(username) {
-    this.message = 'The username ' + username + ' is not available or already taken.';
-    var err = new Error(this.message);
-    err.name = this.constructor.name;
-    this.stack = err.stack;
-  }
-  UsernameNotAvailable.prototype = new Error();
+function UsernameNotAvailable(username) {
+  this.message = 'The username ' + username + ' is not available or already taken.';
+  var err = new Error(this.message);
+  err.name = this.constructor.name;
+  this.stack = err.stack;
+}
+UsernameNotAvailable.prototype = new Error();
 
-  exports.UserNotFound = UserNotFound;
-  exports.UsernameNotAvailable = UsernameNotAvailable;
-}());
+exports.UserNotFound = UserNotFound;
+exports.UsernameNotAvailable = UsernameNotAvailable;
